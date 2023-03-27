e16ee654ec03bb95c25c5c468278e048327079a7
Asher <asherwoodrae@gmail.com>
2023-03-27 01:21:42 +0100

    rainbow colouring, individual gridsquare selection

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// ADMIN
+
 console.log("hello weenies <3");
 
 let body = document.body;
 const container = document.getElementById('gridContainer');
+
+
+// GRID CREATION
 
 function createGrid(gridSize) {
     for (let i = 0; i < gridSize; i++){ 
@@ -9,11 +14,26 @@ function createGrid(gridSize) {
         row.className = "row"; 
         for(let x = 1; x <= gridSize; x++){ 
             let cell = document.createElement("div"); 
-            cell.className = "gridsquare"; 
-            // cell.innerText = (i * gridSize) + x;
+            cell.className = "cell"; 
+            cell.addEventListener('mouseover', colouring)
             row.appendChild(cell); 
         } 
         container.appendChild(row); 
-      } 
+      }
+}
+createGrid(16);
 
-    }
+
+// COLOUR
+
+let gridSquare = document.getElementsByClassName('row', 'cell');
+
+function colouring(e) {
+    let color = `rgb(${[0, 0, 0].map(channel => {
+        return Math.floor(Math.random() * 256);
+    }).join(',')})`
+    Object.assign(e.target.style, {
+        backgroundColor : color,
+        opacity : 0.25
+    });
+}
