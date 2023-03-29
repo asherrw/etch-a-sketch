a3e60e87ee676843db56a341e42016f90d7269e0
Asher <asherwoodrae@gmail.com>
2023-03-29 23:59:35 +0100

    improved grid generation and DOM layout

--- index.js
+++ index.js
@@ -3,37 +3,135 @@
 console.log("hello weenies <3");
 
 let body = document.body;
-const container = document.getElementById('gridContainer');
+
+// Buttons
+const rainbow = document.getElementById('rainbow');
+const green = document.getElementById('green');
+const reset = document.getElementById('reset');
+
 
 
 // GRID CREATION
 
-function createGrid(gridSize) {
+// Default Grid
+// function createGrid(gridSize) {
+//     for (let i = 0; i < gridSize; i++){ 
+//         let row = document.createElement("div"); 
+//         row.className = "row"; 
+//         for(let x = 1; x <= gridSize; x++){ 
+//             let cell = document.createElement("div"); 
+//             cell.className = "cell"; 
+//             row.appendChild(cell); 
+//         } 
+//         container.appendChild(row); 
+//       }
+// }
+
+function createGrid(size) {
+
+    let container = document.querySelector('.gridContainer');
+    let squares = container.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        // square.addEventListener("mouseover", coloring);
+        square.style.backgroundColor = "white";
+        container.insertAdjacentElement("beforeend", square);
+    } 
+}
+
+createGrid(16);
+
+// Change grid size
+function changeGridSize(input){
+    if (input < 2) {
+        return console.log("That number is too low of a number you idiotic clownfish");
+    } else if (input > 100) {
+        return console.log("That number is too high you absolute codfish");
+    } else {
+    createGrid(input);
+    }
+}
+
+// Stained Glass Mosaic Grid
+function createGridRainbow(gridSize) {
     for (let i = 0; i < gridSize; i++){ 
         let row = document.createElement("div"); 
         row.className = "row"; 
         for(let x = 1; x <= gridSize; x++){ 
             let cell = document.createElement("div"); 
             cell.className = "cell"; 
-            cell.addEventListener('mouseover', colouring)
+            cell.addEventListener('mouseover', colouringRainbow)
             row.appendChild(cell); 
         } 
-        container.appendChild(row); 
+        container.appendChild(row);
       }
 }
-createGrid(16);
+
+// Black Pen
+function createGridBlack(gridSize) {
+    for (let i = 0; i < gridSize; i++){ 
+        let row = document.createElement("div"); 
+        row.className = "row"; 
+        for(let x = 1; x <= gridSize; x++){ 
+            let cell = document.createElement("div"); 
+            cell.className = "cell"; 
+            cell.addEventListener('mouseover', colouringBlack)
+            row.appendChild(cell); 
+        } 
+        container.appendChild(row);
+      }
+}
+
+// Green
+function createGridGreen(gridSize) {
+    for (let i = 0; i < gridSize; i++){ 
+        let row = document.createElement("div"); 
+        row.className = "row"; 
+        for(let x = 1; x <= gridSize; x++){ 
+            let cell = document.createElement("div"); 
+            cell.className = "cell"; 
+            cell.addEventListener('mouseover', colouringGreen)
+            row.appendChild(cell); 
+        } 
+        container.appendChild(row);
+      }
+}
 
 
-// COLOUR
+// COLOUR FUNCTIONS
 
-let gridSquare = document.getElementsByClassName('row', 'cell');
-
-function colouring(e) {
+// Rainbow Stained Glass Mosaic Mode
+function colouringRainbow(e) {
     let color = `rgb(${[0, 0, 0].map(channel => {
         return Math.floor(Math.random() * 256);
     }).join(',')})`
     Object.assign(e.target.style, {
         backgroundColor : color,
         opacity : 0.25
+    });
+}
+
+// Blue Mode
+function colouringBlue(e) {
+    let a = Math.random() * 254;
+    let b = Math.floor(a / 8) * 8;
+    let color = `rgb(${[0, 0, b]})`;
+    Object.assign(e.target.style, {
+        backgroundColor : color,
+    });
+}
+
+// Green Mode
+function colouringGreen(e) {
+    let a = Math.random() * 254;
+    let b = Math.floor(a / 8) * 8;
+    let color = `rgb(${[0, b, 0]})`;
+    Object.assign(e.target.style, {
+        backgroundColor : color,
     });
 }
