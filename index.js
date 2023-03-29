d09ae1c97770169a1d74007c9bf585cfb63fcbfa
Asher <asherwoodrae@gmail.com>
2023-03-30 00:24:18 +0100

    added working colour buttons

--- index.js
+++ index.js
@@ -9,24 +9,6 @@ const rainbow = document.getElementById('rainbow');
 const green = document.getElementById('green');
 const reset = document.getElementById('reset');
 
-
-
-// GRID CREATION
-
-// Default Grid
-// function createGrid(gridSize) {
-//     for (let i = 0; i < gridSize; i++){ 
-//         let row = document.createElement("div"); 
-//         row.className = "row"; 
-//         for(let x = 1; x <= gridSize; x++){ 
-//             let cell = document.createElement("div"); 
-//             cell.className = "cell"; 
-//             row.appendChild(cell); 
-//         } 
-//         container.appendChild(row); 
-//       }
-// }
-
 function createGrid(size) {
 
     let container = document.querySelector('.gridContainer');
@@ -38,7 +20,7 @@ function createGrid(size) {
     let amount = size * size;
     for (let i = 0; i < amount; i++) {
         let square = document.createElement('div');
-        // square.addEventListener("mouseover", coloring);
+        square.addEventListener("mouseover", coloring);
         square.style.backgroundColor = "white";
         container.insertAdjacentElement("beforeend", square);
     } 
@@ -57,49 +39,23 @@ function changeGridSize(input){
     }
 }
 
-// Stained Glass Mosaic Grid
-function createGridRainbow(gridSize) {
-    for (let i = 0; i < gridSize; i++){ 
-        let row = document.createElement("div"); 
-        row.className = "row"; 
-        for(let x = 1; x <= gridSize; x++){ 
-            let cell = document.createElement("div"); 
-            cell.className = "cell"; 
-            cell.addEventListener('mouseover', colouringRainbow)
-            row.appendChild(cell); 
-        } 
-        container.appendChild(row);
-      }
+// Function to make the divs change color
+function coloring() {
+    if(color == 'random'){
+        this.style.backgroundColor = `rgb(${[0, 0, 0,].map(channel => {
+            return Math.floor(Math.random() * 400);
+        })})`;
+    } else {
+    this.style.backgroundColor = color;
+    }
 }
 
-// Black Pen
-function createGridBlack(gridSize) {
-    for (let i = 0; i < gridSize; i++){ 
-        let row = document.createElement("div"); 
-        row.className = "row"; 
-        for(let x = 1; x <= gridSize; x++){ 
-            let cell = document.createElement("div"); 
-            cell.className = "cell"; 
-            cell.addEventListener('mouseover', colouringBlack)
-            row.appendChild(cell); 
-        } 
-        container.appendChild(row);
-      }
-}
+// Making color buttons work
 
-// Green
-function createGridGreen(gridSize) {
-    for (let i = 0; i < gridSize; i++){ 
-        let row = document.createElement("div"); 
-        row.className = "row"; 
-        for(let x = 1; x <= gridSize; x++){ 
-            let cell = document.createElement("div"); 
-            cell.className = "cell"; 
-            cell.addEventListener('mouseover', colouringGreen)
-            row.appendChild(cell); 
-        } 
-        container.appendChild(row);
-      }
+let color = 'teal';
+
+function changeColor(option){
+    color = option;
 }
 
 
@@ -113,25 +69,5 @@ function colouringRainbow(e) {
     Object.assign(e.target.style, {
         backgroundColor : color,
         opacity : 0.25
-    });
-}
-
-// Blue Mode
-function colouringBlue(e) {
-    let a = Math.random() * 254;
-    let b = Math.floor(a / 8) * 8;
-    let color = `rgb(${[0, 0, b]})`;
-    Object.assign(e.target.style, {
-        backgroundColor : color,
-    });
-}
-
-// Green Mode
-function colouringGreen(e) {
-    let a = Math.random() * 254;
-    let b = Math.floor(a / 8) * 8;
-    let color = `rgb(${[0, b, 0]})`;
-    Object.assign(e.target.style, {
-        backgroundColor : color,
     });
 }
